3bb15f03cf0dd72bf2dc3a081a338ce70c2dcd92
Steve Tingiris <steve@dabblelab.com>
2018-08-27 07:23:37 -0400

    added example config file

---
+++ example_config.js
@@ -0,0 +1,8 @@
+const config = {};
+
+// twilio config
+config.twilio = {};
+config.twilio.accountSid = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+config.twilio.authToken = "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
+
+module.exports = config;
