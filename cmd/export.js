4f25ba9d0391d6cc0f9f704df07e9ece9a8b9737
Mohammad Khalid <mohdkhalid0807@gmail.com>
2018-09-22 19:16:09 +0530

    added export script into cli

--- cmd/export.js
+++ cmd/export.js
@@ -1,18 +1,24 @@
 const ora = require('ora')
 const tag = require('../lib/tag');
+const files = require('../lib/files');
 
 module.exports = async (args) => {
-  const spinner = ora().start()
+  const spinner = ora().start('Exporting assistant...')
 
   try {
 
     const sid = args.assistant
+    //console.log(sid);
+    //const assistant = await tag.exportAssistant(sid);
+    const filename = await files.createAssistantJSONFile(sid);
+    const assistant = await tag.exportAssistant(sid,filename);
 
     spinner.stop()
 
-    //TODO: finish the export command
+    console.log(`File exported in ${filename}`);
 
-    console.log(`This command is not implemented yet.`)
+    //TODO: finish the export command
+    //console.log(assistant);
 
   } catch (err) {
     spinner.stop()
