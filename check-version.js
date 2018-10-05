0bc242af61e98db30bdca0987151f0ac2821af5b
Mohammad Khalid <mohdkhalid0807@gmail.com>
2018-10-05 17:55:59 +0530

    Closes #12 - detect required node version

--- check-version.js
+++ check-version.js
@@ -4,6 +4,7 @@ const current_version = `'${process.version.substr(1,process.version.length)}'`,
       expected_version = `'${package.engines.node.substr(2,package.engines.node.length)}'`;
 
 
+      
 if(!(current_version>=expected_version)){
     console.error(`Node 8.10 or later is required.`);
     process.exit(1);
