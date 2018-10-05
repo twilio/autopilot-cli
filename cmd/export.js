c18fede6f3c79f7c281464aba1da355190454829
Mohammad Khalid <mohdkhalid0807@gmail.com>
2018-10-05 17:25:09 +0530

    #12 - detect required node version on install

--- cmd/export.js
+++ cmd/export.js
@@ -1,9 +1,15 @@
 
 const ta = require('../lib/twilio-assistant');
 const files = require('../lib/files');
+const _ = require('lodash');
+const inquirer = require('inquirer');
+const ora = require('ora');
+
+
 
 module.exports = async (args) => {
-  //const spinner = ora().start(`Exporting assistant...`)
+
+  const spinner = await ora().start(`Getting assistant List...\n`)
 
   try {
 
@@ -12,9 +18,44 @@ module.exports = async (args) => {
     //const assistant = await ta.exportAssistant(sid);
     //const filename = await files.createAssistantJSONFile(sid);
     const profile = args.profile || "default"
-    const assistant = await ta.exportAssistant(profile);
 
-    //spinner.stop()
+    const client = await require('../lib/twilio-assistant/client')(profile);
+
+
+    await client.preview.understand
+    .assistants
+    .list().then((assistants) => {
+
+      let choices = [];
+      if(assistants.length){
+        for (let i = 0; i < assistants.length; i++) {
+          choices.push(assistants[i].uniqueName);
+          if (i === assistants.length - 1) {
+            spinner.stop();
+            inquirer.prompt([
+              {
+                type: 'list',
+                name: 'assistantName',
+                message: 'Choose your assistant: ',
+                choices: choices
+              }
+            ]).then(async (answer) => {
+              let seletedAssistant = answer.assistantName
+  
+              //const f_index = await _.findIndex(assistants, { uniqueName: seletedAssistant }),
+                //sel_assistant = assistants[f_index];
+              
+                const assistant = await ta.exportAssistant(seletedAssistant,profile);
+                console.log(`File exported in ${assistant.filename}`);
+            })
+          }
+        }
+      }else{
+        console.log('no assistants.');
+        spinner.stop()
+      }
+      
+    })
 
     //console.log(`File exported in ${filename}`);
 
@@ -22,7 +63,7 @@ module.exports = async (args) => {
     //console.log(assistant);
 
   } catch (err) {
-    //spinner.stop()
+    spinner.stop()
     
     console.error(err)
   }
--- cmd/list.js
+++ cmd/list.js
@@ -1,14 +1,24 @@
 const ta = require('../lib/twilio-assistant');
+const ora = require('ora');
 
 module.exports = async (args) => {
 
   try {
     const profile = args.profile || "default";
-    const assistants = ta.listAssistants(profile)
+
+    const spinner = await ora().start('Getting assistants...\n')
+    const assistants = await ta.listAssistants(profile);
+
+    for( let i = 0 ; i < assistants.length ; i++){
+        console.log(`${assistants[i].sid} ${assistants[i].uniqueName}`)
+        if(i === assistants.length - 1){
+            spinner.stop();
+        }
+    }
 
 
   } catch (err) {
-    //spinner.stop()
+    spinner.stop()
     
     console.error(err)
   }
